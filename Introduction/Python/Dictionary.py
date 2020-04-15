9480915b4eafd79a44dd06f93286d4ba1cc4e66e
AdityaBag <adityabag99@gmail.com>
2020-04-15 21:03:36 +0530

    .\Introduction\Python\Dictionary.py

---
+++ Introduction/Python/Dictionary.py
@@ -0,0 +1,16 @@
+MonthList = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+ }
+a = input("Month")
+print(MonthList.get(str(a)))
